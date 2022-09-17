846db7d47f9faff98ecb69716ea533aab12dcc60
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2022-09-17 17:44:33 +0900

    2022/09/17

--- Program.cs
+++ Program.cs
@@ -11,110 +11,115 @@ namespace Calculator
 			double k = 0;
 			string r = "r";
 
-			while( true)
+			while (true)
 			{
-				if ( r == "" ) {
-					string right = k.ToString();
+				if (r == "")
+				{
+					string left = k.ToString();
 
-					Console.WriteLine( "何算" );
-					string aida = Console.ReadLine( );
-					Console.WriteLine( "右" );
-					Console.WriteLine( right );
-					Console.WriteLine( "左" );
-					string left = Console.ReadLine( );
+					Console.WriteLine("何算");
+					string aida = Console.ReadLine();
+					Console.WriteLine("左");
+					Console.WriteLine(left);
+					Console.WriteLine("右");
+					string right = Console.ReadLine();
 
-					if ( aida == "t")
+					if (aida == "t")
 					{
-						k = plus(right, left);
-					}else if ( aida == "h")
+						k = plus(left, right);
+					}
+					else if (aida == "h")
 					{
-						k = minus(right, left);
-					}else if ( aida == "k")
+						k = minus(left, right);
+					}
+					else if (aida == "k")
 					{
-						k = multiplication(right, left);
-					}else if ( aida == "w")
+						k = multiplication(left, right);
+					}
+					else if (aida == "w")
 					{
-						k = division(right, left);
+						k = division(left, right);
 					}
 
-					Console.WriteLine( );
-					Console.WriteLine( "答えは" );
-					Console.WriteLine( k );
-					Console.WriteLine( );
+					Console.WriteLine();
+					Console.WriteLine("答えは");
+					Console.WriteLine(k);
+					Console.WriteLine();
 
-				}else if ( r == "r")
+				}
+				else if (r == "r")
 				{
 					k = 0;
 
 					Console.WriteLine("何算");
 					string aida = Console.ReadLine();
-					Console.WriteLine("右");
-					string right = Console.ReadLine();;
 					Console.WriteLine("左");
 					string left = Console.ReadLine();
+					Console.WriteLine("右");
+					string right = Console.ReadLine();
 
 					if (aida == "t")
 					{
-						k = plus(right, left);
+						k = plus(left, right);
 					}
 					else if (aida == "h")
 					{
-						k = minus(right, left);
+						k = minus(left, right);
 					}
 					else if (aida == "k")
 					{
-						k = multiplication(right, left);
+						k = multiplication(left, right);
 					}
 					else if (aida == "w")
 					{
-						k = division(right, left);
+						k = division(left, right);
 					}
 
-					Console.WriteLine( );
-					Console.WriteLine( "答えは" );
-					Console.WriteLine( k );
-					Console.WriteLine( );
+					Console.WriteLine();
+					Console.WriteLine("答えは");
+					Console.WriteLine(k);
+					Console.WriteLine();
 				}
 
-				Console.WriteLine( "リセット" );
-				r = Console.ReadLine( );
-				Console.WriteLine( );
+				Console.WriteLine("リセット");
+				r = Console.ReadLine();
+				Console.WriteLine();
 			}
 		}
 
-		static double plus( string right, string left)
+		static double plus(string left, string right)
 		{
-			double m = double.Parse( right );
-			double h = double.Parse( left );
-			double answer = m + h;
+			double h = double.Parse(left);
+			double m = double.Parse(right);
+			double answer = h + m;
 
 			return answer;
 		}
 
-		static double minus( string right, string left)
+		static double minus(string left, string right)
 		{
-			double m = double.Parse(right);
 			double h = double.Parse(left);
-			double answer = m - h;
+			double m = double.Parse(right);
+			double answer = h - m;
 
 			return answer;
 		}
 
-		static double multiplication( string right, string left)
+		static double multiplication(string left, string right)
 		{
-			double m = double.Parse(right);
-			double h = double.Parse(left);
-			double answer = m * h;
+			double h = double.Parse(right);
+			double m = double.Parse(left);
+			double answer = h * m;
 
 			return answer;
 		}
 
-		static double division( string right, string left)
+		static double division(string left, string right)
 		{
-			double m = double.Parse(right);
 			double h = double.Parse(left);
-			double answer = m / h;
-			 
+			double m = double.Parse(right);
+			double answer = h / m;
+
 			return answer;
 		}
 	}
